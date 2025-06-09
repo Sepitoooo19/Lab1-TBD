ccc8ae76fa6cfdfd79c914ad39c80a258740472a
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2025-06-08 20:30:57 -0400

    Elimino Ubi (para probar)

--- lab1/src/main/java/bdavanzadas/lab1/entities/ClientEntity.java
+++ lab1/src/main/java/bdavanzadas/lab1/entities/ClientEntity.java
@@ -22,7 +22,6 @@ public class ClientEntity {
     private String email;
     private String phone;
     private String address;
-    private String ubication;
 
 
     private int userId; // Relación con UserEntity
